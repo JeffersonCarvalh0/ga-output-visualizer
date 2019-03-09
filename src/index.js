0863107917c78de6a51007cb4c0a351be525d79d
Jefferson <jeffersonhcarvalho@gmail.com>
2019-03-09 11:24:53 -0300

    add prettier before commit and use hooks

--- src/index.js
+++ src/index.js
@@ -1,105 +1,72 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React, { useState } from 'react'
+import { render } from 'react-dom'
 
-import { CardPanel, Input, Table } from 'react-materialize';
+import { CardPanel, Input, Table } from 'react-materialize'
 
-function TimetableRow(props) {
-    const cells = props.labels.map((e) => <td> { e } </td>);
+const TimetableRow = props => {
+  const cells = props.labels.map(e => <td> {e} </td>)
+
+  return <tr>{cells}</tr>
+}
+
+const Timetable = props => {
+  if (props.labels && props.periodNum) {
+    const periodNum = parseInt(props.periodNum)
+    const labels = props.labels.split(' ')
+    const days = Math.trunc(labels.length / periodNum)
+    const heads = []
+    const rows = []
+
+    heads.push(<th />)
+    for (let i = 0; i < days; ++i) heads.push(<th> Day {i + 1} </th>)
+
+    for (let i = 0; i < periodNum; ++i) {
+      const currentRow = [<span> Period {i + 1} </span>]
+      for (let j = i; j < labels.length; j += periodNum) currentRow.push(labels[j])
+      rows.push(<TimetableRow labels={currentRow} />)
+    }
 
     return (
-        <tr>
-            { cells }
-        </tr>
-    );
+      <div className="section">
+        <h4> Timetable </h4>
+        <div className="divider" />
+        <CardPanel className="z-depth-5">
+          <Table centered hoverable>
+            <thead>
+              <tr>{heads}</tr>
+            </thead>
+            <tbody>{rows}</tbody>
+          </Table>
+        </CardPanel>
+      </div>
+    )
+  }
+
+  return null
 }
 
-function Timetable(props) {
-    if (props.labels && props.periodNum) {
-        const periodNum = parseInt(props.periodNum);
-        const labels = props.labels.split(' ');
-        const days = Math.trunc(labels.length / periodNum);
-        const heads = [];
-        const rows = [];
+const App = props => {
+  const [labels, setLabels] = useState('')
+  const [periodNum, setperiodNum] = useState(0)
 
-        heads.push(<th></th>);
-        for (let i = 0; i < days; ++i) heads.push(<th> Day {i + 1} </th>)
+  return (
+    <>
+      <nav>
+        <div className="nav-wrapper indigo">
+          <span className="brand-logo center"> Output visualizer </span>
+        </div>
+      </nav>
 
-        console.log(labels);
-        for (let i = 0; i < periodNum; ++i) {
-            const currentRow = [<span> Period {i + 1} </span>];
-            for (let j = i; j < labels.length; j += periodNum)
-                currentRow.push(labels[j]);
-            rows.push(<TimetableRow labels={currentRow} />)
-        }
+      <div className="container">
+        <div className="section">
+          <Input name="labels" onChange={e => setLabels(e.target.value)} label="Tuple labels" />
+          <Input name="periodNum" type="number" onChange={e => setPeriodNum(e.target.value)} label="Periods per day" />
+        </div>
 
-        return (
-            <div className="section">
-                <h4> Timetable </h4>
-                <div className="divider"></div>
-                <CardPanel className="z-depth-5">
-                    <Table centered hoverable>
-                        <thead>
-                            <tr>
-                                { heads }
-                            </tr>
-                        </thead>
-                        <tbody>
-                            { rows }
-                        </tbody>
-                    </Table>
-                </CardPanel>
-            </div>
-        );
-    }
-
-    return null;
+        <Timetable labels={labels} periodNum={periodNum} />
+      </div>
+    </>
+  )
 }
 
-class App extends React.Component {
-    state = {
-        labels: "",
-        periodNum: null,
-    };
-
-    handleInput = ({ target: { name, value } }) => {
-        this.setState({
-            [name]: value
-        });
-    }
-
-    render() {
-        return(
-            <React.Fragment>
-                <nav>
-                    <div className="nav-wrapper indigo">
-                        <span className="brand-logo center"> Output visualizer </span>
-                    </div>
-                </nav>
-
-                <div className="container">
-                    <div className="section">
-                        <Input
-                            name="labels"
-                            onChange={this.handleInput}
-                            label="Tuple labels"
-                        />
-
-                        <Input
-                            name="periodNum"
-                            type="number"
-                            onChange={this.handleInput}
-                            label="Periods per day"
-                        />
-                    </div>
-
-                    <Timetable
-                        labels={this.state.labels}
-                        periodNum={this.state.periodNum}
-                    />
-                </div>
-            </React.Fragment>
-        );
-    }
-}
-
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'))
